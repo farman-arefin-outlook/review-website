b26ff107d5640a47343d36c909bb64ed545b31dc
Tamim <farman.arefin1@outlook.com>
2021-10-05 00:23:41 +0600

    Resolve carosel

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,47 +1,51 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
-import Header from '../Header/Header';
+import pic1 from '../slider/pic-1.jpg';
+import pic2 from '../slider/pic-2.jpg';
+import pic3 from '../slider/pic-3.jpg';
 
 const Home = () => {
     return (
         <div>
-            <Carousel>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="../slider/pic-1.jpg"
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="../slider/pic-2.jpg"
-                        alt="Second slide"
-                    />
+            <div className="container">
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={pic1}
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>Introduction to Structered Programming Language.</h3>
+                            <p>Structerd Programming or Modular Programming is now a programming paradigm...</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={pic2}
+                            alt="Second slide"
+                        />
 
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="../slider/pic-3.jpg"
-                        alt="Third slide"
-                    />
+                        <Carousel.Caption>
+                            <h3>Operating System and System Programming</h3>
+                            <p>Operating System is major part of Computer Science. Now most giant company needed special knowledge about Operating System</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={pic3}
+                            alt="Third slide"
+                        />
 
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+                        <Carousel.Caption>
+                            <h3>Computer Networking</h3>
+                            <p>Computer Networking is now play an important role in the field of computer science...</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
         </div>
     );
 };
