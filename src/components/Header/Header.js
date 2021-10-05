33060988aca1cc50cca30ec0b36136e98933ddf9
Tamim <farman.arefin1@outlook.com>
2021-10-05 22:42:50 +0600

    commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
             <>
                 <Navbar bg="dark" variant="dark">
                     <Container>
-                        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                        <Navbar.Brand href="#home">Tamim Computer Academy</Navbar.Brand>
                         <Nav className="me-auto">
                             <Nav.Link href="/home">Home</Nav.Link>
                             <Nav.Link href="/courses">Courses</Nav.Link>
